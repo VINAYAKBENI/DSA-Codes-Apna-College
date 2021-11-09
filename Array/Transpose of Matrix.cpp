6902e668c9078941e829a5e0656644acb5ded427
VINAYAK BENI <benivinayak2002@gmail.com>
2021-11-09 18:45:54 +0530

    Transpose of Matrix

---
+++ Array/Transpose of Matrix.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define e cout << endl;
+
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputvs.in", "r", stdin);
+    freopen("outputvs.in", "w", stdout);
+#endif
+    int n;
+    cin>>n;
+    int a[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    //Transpose of a Matrix
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(a[i][j],a[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<<" ";
+        }
+        e
+    }
+    return 0;
+}
