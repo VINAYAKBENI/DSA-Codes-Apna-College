013b3ed511a5e0651b456fc26cccef86a136697d
VINAYAK BENI <benivinayak2002@gmail.com>
2021-12-12 17:55:38 +0530

    Postfix Expression Evaulation Using Stack

---
+++ Stack/Postfix Expression Evaulation Using Stack.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define e cout << endl;
+
+using namespace std;
+
+// Postfix Expression Evaulation Using Stack
+int postfix_evaluation(string s)
+{
+
+    stack<int> st;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            st.push(s[i] - '0');
+        }
+        else
+        {
+            int op2 = st.top();
+            st.pop();
+            int op1 = st.top();
+            st.pop();
+
+            switch (s[i])
+            {
+            case '+':
+                st.push(op1 + op2);
+                break;
+
+            case '-':
+                st.push(op1 - op2);
+                break;
+
+            case '*':
+                st.push(op1 * op2);
+                break;
+            case '/':
+                st.push(op1 / op2);
+                break;
+            case '^':
+                st.push(pow(op1, op2));
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputvs.in", "r", stdin);
+    freopen("outputvs.in", "w", stdout);
+#endif
+
+    cout << postfix_evaluation("46+2/5*7+") << endl;
+
+    return 0;
+}
