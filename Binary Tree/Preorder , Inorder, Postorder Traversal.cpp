195734f7fc3849c9af38190ae33a9259b9f2e35d
VINAYAK BENI <benivinayak2002@gmail.com>
2021-12-19 12:47:45 +0530

    Preorder , Inorder, Postorder Traversal

---
+++ Binary Tree/Preorder , Inorder, Postorder Traversal.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define e cout << endl;
+
+using namespace std;
+
+// Class of Binary Tree
+class tree
+{
+public:
+    int data;
+    tree *left;
+    tree *right;
+
+    tree(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Traversals in Binary Trees
+// Pre-Order Traversal
+void preorder(tree *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// In-Order Traversal
+void inorder(tree *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// Post-Order Traversal
+void postorder(tree *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputvs.in", "r", stdin);
+    freopen("outputvs.in", "w", stdout);
+#endif
+
+    tree *root = new tree(1);
+    root->left = new tree(2);
+    root->right = new tree(3);
+    root->left->left = new tree(4);
+    root->left->right = new tree(5);
+    root->right->left = new tree(6);
+    root->right->right = new tree(7);
+    /*        1
+           /    \
+         2       3
+       /  \    /   \
+     4     5  6     7
+
+
+       */
+
+    preorder(root);
+    e
+    inorder(root);
+    e
+    postorder(root);
+    
+    return 0;
+}
