80e53b93d853a5b6ce9cc102e1a2f0b48bfc3177
VINAYAK BENI <benivinayak2002@gmail.com>
2021-12-21 18:15:56 +0530

    Right View Of Binary Tree

---
+++ Binary Tree/Right View Of Binary Tree.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define e cout << endl;
+
+using namespace std;
+
+// Class of Binary Tree
+class tree
+{
+public:
+    int data;
+    tree *left;
+    tree *right;
+
+    tree(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Right View Of Binary Tree
+void right_view(tree *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<tree *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+
+        int n = q.size();
+        for (int i = 0; i < n; i++)
+        {
+            tree *curr = q.front();
+            q.pop();
+
+            if (i == n - 1)
+            {
+                cout << curr->data << " ";
+            }
+            if (curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+            if (curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputvs.in", "r", stdin);
+    freopen("outputvs.in", "w", stdout);
+#endif
+
+    tree *root = new tree(1);
+    root->left = new tree(2);
+    root->right = new tree(3);
+    root->left->left = new tree(4);
+    root->left->right = new tree(5);
+    root->right->left = new tree(6);
+    root->right->right = new tree(7);
+    root->left->left->left = new tree(8);
+
+    cout << "Right View is : ";
+    right_view(root);
+
+    return 0;
+}
