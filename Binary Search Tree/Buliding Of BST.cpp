2cc8a068a1b4b479a990fc5580ba64b38cc160ba
VINAYAK BENI <benivinayak2002@gmail.com>
2021-12-26 18:19:00 +0530

    Buliding Of BST

---
+++ Binary Search Tree/Buliding Of BST.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define e cout << endl;
+
+using namespace std;
+
+// Class of Binary Search Tree
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Inserting a BST
+node *insertBST(node *root, int val)
+{
+    if (root == NULL)
+    {
+        return new node(val);
+    }
+    if (val < root->data)
+    {
+        root->left = insertBST(root->left, val);
+    }
+    else
+    {
+        root->right = insertBST(root->right, val);
+    }
+    return root;
+}
+
+// Inorder Traversal
+void inorder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputvs.in", "r", stdin);
+    freopen("outputvs.in", "w", stdout);
+#endif
+
+    /*
+            5
+          /  \
+         1    7
+          \
+           3
+         /  \
+        2    4
+
+    */
+
+    node *root = NULL;
+    root = insertBST(root, 5);
+    insertBST(root, 1);
+    insertBST(root, 3);
+    insertBST(root, 4);
+    insertBST(root, 2);
+    insertBST(root, 7);
+
+    inorder(root);
+    return 0;
+}
